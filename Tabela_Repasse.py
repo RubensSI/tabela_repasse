db4de1202b1ec6d34ec8beda9a4bfda51a926e84
rubenssi <rubenssi825@gmail.com>
2021-09-07 22:32:26 -0300

    Criei o método para conversão de decimal para binario

--- Tabela_Repasse.py
+++ Tabela_Repasse.py
@@ -8,18 +8,37 @@ class TabelaRepasse():
         if self.ip == '':
             raise ValueError("Ip não enviado.");
 
-        if self.ip_tem_prefixo():
-            pass
+        self.ip_tem_prefixo()
+
+        self.ip_decimal_binario(ip=self.ip)
+
+    def ip_decimal_binario(self, ip=''):
+        if not ip:
+            ip = self.ip
+
+        # A variável bloco_ip se refere ao bloco de endereço
+        bloco_ip = ip.split('.')
+        ip_bin = []
+
+        for bloco in bloco_ip:
+            binario = bin(int(bloco))
+            binario = binario[2:].zfill(8)
+            ip_bin.append(binario)
+
+        ip_bin = '.'.join(ip_bin)
+        return ip_bin
 
     def ip_tem_prefixo(self):
         # 192.168.20.65/24
         ip_prefixo_regexp = re.compile('^[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}/[0-9]{1,2}$')
 
-        if ip_prefixo_regexp(self.ip):
-            print("tem")
-            return True
-        print("N tem")
-        return False
+        if not ip_prefixo_regexp.search(self.ip):
+            return
+
+        divide_ip = self.ip.split('/')
+        self.ip = divide_ip[0]
+        self.prefixo = divide_ip[1]
+
 
 if __name__ == '__main__':
-    pepasse = TabelaRepasse()
+    pepasse = TabelaRepasse(ip='192.168.20.65/24')
